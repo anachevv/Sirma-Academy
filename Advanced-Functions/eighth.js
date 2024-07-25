8a3b35fe6d8116e6e4679676f69010a31d3cc98c
Coding_Jack <36029469+CodingPawn@users.noreply.github.com>
2024-07-25 16:05:12 +0300

    update

--- Advanced-Functions/eighth.js
+++ Advanced-Functions/eighth.js
@@ -1,3 +1,22 @@
+const stringManipulator = {
+    value: '',
+    setValue: function (str) {
+        this.str = str;
+        return this;
+    },
+    toUpperCase: function () {
+        this.str = this.str.toUpperCase();
+        return this;
+    },
+    toLowerCase: function () {
+        this.str = this.str.toLowerCase();
+        return this;
+    },
+    print: function () {
+        console.log(this.str);
+        return this;
+    }
+}
 
 stringManipulator.setValue('Hello')
   .toUpperCase()
